125298d6ecdf62fdce9961aa6de841bf72be921a
LazarievAndrew <lazarev505@gmail.com>
2019-06-24 08:00:48 +0300

    upd1.1
    change of variable names

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var R = prompt('Введите радиус окружности основы цилиндра (в см');
-var H = prompt('Введите высоту цилиндра (в см)');
-var S = Math.PI * Math.pow(R,2);
-var V = S * H;
-var S = (Math.round(S * 100) / 100);
-var V = (Math.round(V * 100) / 100);
+var cylinderBaseRadius = prompt('Введите радиус окружности основы цилиндра (в см)');
+var cylinderHeight = prompt('Введите высоту цилиндра (в см)');
+var cylinderBaseArea = Math.PI * Math.pow(cylinderBaseRadius,2);
+var cylinderVolume = cylinderBaseArea * cylinderHeight;
+var cylinderBaseArea = (Math.round(cylinderBaseArea * 100) / 100);
+var cylinderVolume = (Math.round(cylinderVolume * 100) / 100);
 
-document.write("Обьем цилиндра с площадью основы " + S + "см2, радиусом " + R + "см и высотой " + H + "см равен: " + V +"см3");
+document.write("Обьем цилиндра с площадью основы " + cylinderBaseArea + "см2, радиусом " + cylinderBaseRadius + "см и высотой " + cylinderHeight + "см равен: " + cylinderVolume +"см3");
